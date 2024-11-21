b9154e32b10a788a296b9737f477d509450fce1c
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-11-20 21:41:50 -0300

    components descricao e plataformas

--- landing-page/src/app/page.tsx
+++ landing-page/src/app/page.tsx
@@ -1,9 +1,13 @@
+import { Descricao } from "./components/descrição/descricao";
 import { Header } from "./components/header/header";
+import { Plataformas } from "./components/plataformas/plataformas";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Descricao />
+      <Plataformas />
     </div>
   );
 }
