32182e1598f101019cf781332b3aaf7dd0a85b3b
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-11-20 21:42:42 -0300

    components de plataforma para anunciar

---
+++ landing-page/src/app/components/plataformas/plataformas.tsx
@@ -0,0 +1,10 @@
+import styles from "./plataformas.module.scss";
+export function Plataformas() {
+  return (
+    <div className={styles.Container}>
+      <h3>coloque sua empresa onde a atenção do seu publico está</h3>
+
+      <div></div>
+    </div>
+  );
+}
