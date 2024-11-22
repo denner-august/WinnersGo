53037533ee528567fc792e9670711a8e8fea7d9f
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-11-22 18:37:16 -0300

    pequenas mudanças

--- landing-page/src/app/components/plataformas/plataformas.tsx
+++ landing-page/src/app/components/plataformas/plataformas.tsx
@@ -1,10 +1,37 @@
+import { Benenficios } from "@/app/tools/beneficios/benenficios";
+
+import facebook from "../../../../public/icons/face.png";
+import instagram from "../../../../public/icons/instagram.png";
+import googleads from "../../../../public/icons/googleads.png";
+import linkedin from "../../../../public/icons/linkedin.png";
+
 import styles from "./plataformas.module.scss";
 export function Plataformas() {
   return (
     <div className={styles.Container}>
-      <h3>coloque sua empresa onde a atenção do seu publico está</h3>
+      <h3>
+        coloque <span>sua empresa</span> onde a{" "}
+        <span>atenção do seu publico</span> está
+      </h3>
+      <div className={styles.plataformas}>
+        <Benenficios
+          icon={facebook}
+          description="Com bilhões de usuários ativos, o Facebook conecta sua empresa a um público diversificado e altamente segmentado, perfeito para alcançar seus clientes ideais."
+        />
+        <Benenficios
+          icon={googleads}
+          description="Seja encontrado exatamente quando as pessoas estão procurando pelo que você oferece. Anúncios no Google garantem visibilidade no momento certo"
+        />
+        <Benenficios
+          icon={instagram}
+          description="Alcance seu público com anúncios visuais impactantes, ideais para engajar e fortalecer a presença da sua marca em um ambiente altamente interativo"
+        />
 
-      <div></div>
+        <Benenficios
+          icon={linkedin}
+          description="Alcance profissionais e empresas no maior hub de negócios do mundo. Ideal para quem quer gerar oportunidades no mercado B2B e fortalecer sua autoridade."
+        />
+      </div>
     </div>
   );
 }
--- landing-page/src/app/tools/beneficios/benenficios.tsx
+++ landing-page/src/app/tools/beneficios/benenficios.tsx
@@ -1,17 +1,39 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
 import styles from "./beneficios.module.scss";
 import { StaticImport } from "next/dist/shared/lib/get-img-props";
+
 export interface BenenficiosProps {
-  title: string;
-  icon: StaticImport;
+  title?: string;
+  icon: string | StaticImport; // Permite ícone dinâmico ou estático
+  description: string; // Torna a propriedade opcional
 }
-export function Benenficios(props: BenenficiosProps) {
-  const { title, icon } = props;
+
+export function Benenficios({
+  title,
+  icon,
+  description = "teste",
+}: BenenficiosProps) {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleFlip = () => setIsFlipped(!isFlipped);
 
   return (
-    <div className={styles.Container}>
-      <p>{title}</p>
-      <Image src={icon} alt="icon" />
+    <div
+      className={`${styles.card} ${isFlipped ? styles.flipped : ""}`}
+      onClick={handleFlip}
+    >
+      <div className={`${styles.cardInner}`}>
+        <div className={styles.front}>
+          {title ? <p>{title}</p> : null}
+          <Image src={icon} alt={`${title} icon`} width={50} height={50} />
+        </div>
+        <div className={styles.back}>
+          <p>{description}</p>
+        </div>
+      </div>
     </div>
   );
 }
