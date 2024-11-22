a456effa479d55ce3004b5ea4fbb4690278a5522
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-11-22 18:36:35 -0300

    mudança na descricao dos beneficios

--- landing-page/src/app/components/descrição/descricao.tsx
+++ landing-page/src/app/components/descrição/descricao.tsx
@@ -9,15 +9,30 @@ import tempo from "../../../../public/icons/economizar_tempo.png";
 export function Descricao() {
   return (
     <div className={styles.Container}>
-      <h2>Como o tráfego pago pode ajudar o seu negocio</h2>
+      <h2>
+        Como o tráfego pago pode <span>ajudar o seu negocio</span>
+      </h2>
       <div className={styles.beneficios}>
         <Benenficios
           title=" Atrair Leads Que Realmente Importam"
           icon={leads}
+          description="Conecte-se com pessoas interessadas nos seus produtos ou serviços e transforme visitantes em clientes qualificados."
         />
-        <Benenficios title="economizar Tempo e Dinheiro" icon={tempo} />
-        <Benenficios title="Destacar Sua Marca e Identidade" icon={marca} />
-        <Benenficios title="Aumentar Suas Vendas e Resultados" icon={vendas} />
+        <Benenficios
+          title="economizar Tempo e Dinheiro"
+          icon={tempo}
+          description="Deixe a gestão de tráfego comigo e libere seu tempo para focar no que realmente importa: fazer o seu negócio crescer."
+        />
+        <Benenficios
+          title="Destacar Sua Marca e Identidade"
+          icon={marca}
+          description="Fortaleça a presença do seu negócio e conquiste reconhecimento no mercado com campanhas direcionadas"
+        />
+        <Benenficios
+          title="Aumentar Suas Vendas e Resultados"
+          icon={vendas}
+          description="Transforme cada investimento em tráfego pago em crescimento real para o seu faturamento."
+        />
       </div>
     </div>
   );
