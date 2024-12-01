caab3a498ed3f97d6ebd8e76fbc4114d40b05c8b
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2024-12-01 19:40:23 -0300

    adicao do formulario

--- landing-page/src/app/page.tsx
+++ landing-page/src/app/page.tsx
@@ -1,13 +1,23 @@
 import { Descricao } from "./components/descrição/descricao";
+import { Divulgacao } from "./components/divulgacao/divulgacao";
+import { Form } from "./components/form/formulario";
 import { Header } from "./components/header/header";
 import { Plataformas } from "./components/plataformas/plataformas";
+import { Indicador } from "./tools/indicador/arrow-down";
+
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Descricao />
+      <Divulgacao />
+      <Indicador />
       <Plataformas />
+      <div className={styles.Container}>
+        <Form />
+      </div>
     </div>
   );
 }
